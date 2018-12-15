921d6f7982ba5e946e8ca73594e75a2f5cbca51d
folkertleistra <f.a.leistra@student.rug.nl>
2018-12-15 17:18:09 +0100

    Added new routes to the controller.

--- index.php
+++ index.php
@@ -37,29 +37,14 @@ $nav_template =
             'url' => '/DDWT18_final/'
         ),
         2 => Array(
-            'name' => 'Overview',
-            'url' => '/DDWT18_final'
+            'name' => 'Rooms for rent',
+            'url' => '/DDWT18_final/rentable-rooms/'
         ),
-        3 => Array(
-            'name' => 'My Account',
-            'url' => '/DDWT18_final'
-        ),
-        4 => Array(
-            'name' => 'Register',
-            'url' => '/DDWT18_final'
-        ),
-        5 => Array(
-            'name'=> 'Login',
-            'url' => '/DDWT18_final'
-        ),
-        6 => Array(
-            'name'=> 'Rooms for rent',
-            'url' => '/DDWT18_final'
-        )
     );
 
 /* This section contains all routes */
 if (new_route('/DDWT18_final/', 'get')) {
+    /* page info */
     $page_title = 'Home';
     $navigation = get_navigation($nav_template, 1);
 
@@ -70,7 +55,29 @@ if (new_route('/DDWT18_final/', 'get')) {
     include use_template('main');
 }
 
+/* Page that contains all the rooms that are currently for rent */
+elseif (new_route('/DDWT18_final/rentable-rooms/', 'get' )) {
+    /* page info */
+    $page_title = 'Rooms for rent';
+    $navigation = get_navigation($nav_template, 2);
+    echo 'test';
 
+    include use_template('main');
+}
+
+
+
+elseif (new_route('/DDWT18_final/test/', 'get')) {
+    /* page info */
+    $page_title = 'Home';
+    $navigation = get_navigation($nav_template, 1);
+
+    /*page content */
+    $page_subtitle = 'Living on my own!';
+    $page_content = 'Boom Boom Boom Boom, I want you in my room!';
+
+    include use_template('main');
+}
 
 else {
     http_response_code(404);
