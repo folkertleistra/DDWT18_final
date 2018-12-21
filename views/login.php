7c48bf456d43fb828e92bb20f1c23aff06e1d884
thijmendam <thijmenthijmen@live.nl>
2018-12-21 21:49:32 +0100

    A lot! Redesign register page now 100% complete and responsive. Redone login page as well because CSS was better formatted.

--- views/login.php
+++ views/login.php
@@ -2,32 +2,35 @@
 <html lang="en">
     <head>
         <?= $head_upper_content ?>
-        <link rel="stylesheet" href="/DDWT18_final/css/login-register-backup.css">
+        <link rel="stylesheet" href="/DDWT18_final/css/login-register.css">
         <title><?= $page_title ?></title>
     </head>
     <body>
-
 
         <!-- Content -->
         <div class="container">
             <div class="row justify-content-center">
 
                 <div class="col">
-                    <div class="form-box">
-                        <div class="login-register-box">
 
+                    <!-- White wrapper box -->
+                    <div class="wrapper-box">
+                        <div class="login-box">
+
+                            <!-- Top section -->
                             <div class="apr-logo">
                                 <img src="../resources/logo/apartrent-logo-square.png">
                             </div>
-
-                            <div class="heading">
-                                <h4>Sign In</h4>
+                            <div class="upper-text">
+                                <h2 class="heading">Sign In</h2>
                                 <div class="alt-option-div">
                                     <a href="/DDWT18_final/register/"> Register Now </a>
                                 </div>
                             </div>
+
+                            <!-- Form -->
                             <form action="/DDWT18_final/login/" method="POST" class="sign-in-register-form">
-                                <div class="form-row-sign-in">
+                                <div class="form-row">
                                     <label for="username"  class="sign-in-register-label" >Username</label>
                                     <input type="text" name="username" placeholder="Enter Username" class="form-input" required>
                                 </div>
@@ -35,17 +38,17 @@
                                     <label for="password" class="sign-in-register-label" >Password</label>
                                     <input type="password" name="password" placeholder="Enter Password" class="form-input" required>
                                 </div>
-                                <div class="form-row-sign-in">
-                                    <button type="submit" id="sign-in-btn">Sign In</button>
+                                <div class="form-row">
+                                    <button type="submit" id="form-btn">Sign In</button>
                                 </div>
-                                <a href="/DDWT18_final/"> Return to the Homepage</a>
-
                             </form>
+
+                            <!-- Return home link -->
+                            <a href="/DDWT18_final/" class="return-home"> Return to the Homepage</a>
 
                         </div>
                     </div>
                 </div>
-
             </div>
         </div>
 
--- views/register.php
+++ views/register.php
@@ -51,17 +51,71 @@
 
                         <!-- Account Details -->
                         <h3 class="section-heading">Account Details</h3>
+                        <div class="form-row">
+                            <label for="username"  class="sign-in-register-label">Username</label>
+                            <input type="text" name="username" placeholder="Enter username" class="form-input" required>
+                        </div>
+                        <div class="form-row">
+                            <label for="password"  class="sign-in-register-label">Password</label>
+                            <input type="password" name="password" placeholder="Enter password" class="form-input" required>
+                        </div>
+                        <div class="form-row">
+                            <label for="rt-password"  class="sign-in-register-label">Retype password </label>
+                            <input type="password" name="rt-password" placeholder="Enter password" class="form-input" required>
+                        </div>
+                        <div class="form-row">
+                            <label for="email"  class="sign-in-register-label">E-mail</label>
+                            <input type="email" name="email" placeholder="mail@mail.com" class="form-input" required>
+                        </div>
 
                     </div>
 
                     <!-- Right column lg/md, bottom column sm/xs -->
                     <div class="col-lg-6 register-col">
+                        <!-- Personal details -->
                         <h3 class="section-heading">Personal Details</h3>
+                         <div class="form-row">
+                            <label for="firstname"  class="sign-in-register-label">First Name</label>
+                            <input type="text" name="firstname" placeholder="First Name" class="form-input" required>
+                        </div>
+                        <div class="form-row">
+                            <label for="lastname" class="sign-in-register-label">Last Name</label>
+                            <input type="text" name="lastname" placeholder="Last Name" class="form-input" required>
+                        </div>
+                        <div class="form-row">
+                            <label for="birthdate" class="sign-in-register-label">Date of Birth</label>
+                            <input type="date" name="birthdate"  class="form-input" required>
+                        </div>
+                        <div class="form-row">
+                            <label for="phone" class="sign-in-register-label">Phone Number</label>
+                            <input type="number" name="phone" placeholder="0612345678" class="form-input" required>
+                        </div>
+                        <div class="form-row">
+                            <label for="language"  class="sign-in-register-label">Language</label>
+                            <input type="text" name="language" placeholder="Language" class="form-input" required>
+                        </div>
+                        <div class="form-row">
+                            <label for="occupation"  class="sign-in-register-label">Occupation</label>
+                            <input type="text" name="occupation" placeholder="Occupation" class="form-input" required>
+                        </div>
+                    </div>
+
+                    <!-- Bottom column -->
+                    <div class="col-lg-12 bot-col">
+                        <h3 class="section-heading">Biography</h3>
+                        <div class="form-row">
+                            <textarea class="form-input" placeholder="Tell us a bit more about yourself!" name="biography" required></textarea>
+                        </div>
+
+                        <!-- Register / return to home -->
+                        <div class="form-row">
+                            <button type="submit" id="form-btn">Register</button>
+                        </div>
+                        <a href="/DDWT18_final/" class="return-home"> Return to the Homepage</a>
                     </div>
 
                 </div>
             </div>
-
         </div>
     </form>
 
