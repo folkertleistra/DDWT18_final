53db0882925011e9d1708012b462167cab885420
folkertleistra <f.a.leistra@student.rug.nl>
2019-01-03 13:47:38 +0100

    added my account view and a fade for erroor messages

--- views/my_account.php
+++ views/my_account.php
@@ -1,19 +1,31 @@
 <!doctype html>
 <html lang="en">
-<head>
-    <?= $head_upper_content ?>
-    <link rel="stylesheet" href="/DDWT18_final/css/home.css">
-    <title><?= $page_title ?></title>
-</head>
-<body>
-<!-- Navigation Menu -->
-<?= $navigation ?>
+    <head>
+        <?= $head_upper_content ?>
+        <link rel="stylesheet" href="/DDWT18_final/css/account.css">
+        <title><?= $page_title ?></title>
+    </head>
+    <body>
+        <!-- Navigation Menu -->
+        <?= $navigation ?>
+
+        <!-- Content -->
+
+        <div class="jumbotron jumbotron-fluid myaccount-jumbo">
+            <div class="container hero-container">
+                <div class="hero-text">
+                    <h1 class="display-4">My Account</h1>
+                    <h2 class="display-6">Lorem ipsum di amor</h2>
+                </div>
+            </div>
+        </div>
 
 
-<!-- Footer -->
+        <!-- Footer -->
 
-<?= $footer ?>
-<?= $error_msg ?>
-<?= $imported_scripts ?>
-</body>
+        <?= $footer ?>
+        <!-- Error message -->
+        <div class="error-fade"><?php if (isset($error_msg)){echo $error_msg;} ?></div>
+        <?= $imported_scripts ?>
+    </body>
 </html>
