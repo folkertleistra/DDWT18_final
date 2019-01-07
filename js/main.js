5429eff9d15ba13a3ad0aff0e09ad430502566e2
hylkevdveen <h.f.van.der.veen@student.rug.nl>
2019-01-07 17:09:23 +0100

    fixed parameter display in url

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 // Clean GET from URL
-let clean_url = window.location['href'].split('?')[0];
-window.history.pushState(null, null, clean_url);
+let url_string = window.location['href'];
+let short_url = window.location['href'].split('?')[0];
+let url = new URL(url_string);
+let id = url.searchParams.get("id");
+if (id == null) {
+    window.history.pushState(null, null, short_url);
+} else {
+    let clean_url = short_url.concat(`?id=${id}`);
+    window.history.pushState(null, null, clean_url);
+}
